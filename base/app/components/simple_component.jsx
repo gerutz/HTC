e084547aab24204f39a8a7fb9a2f4ac606006abf
German Utz <germanutz@gmail.com>
2016-07-24 08:21:04 -0300

    actualizamos archivo base

---
+++ base/app/components/simple_component.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      controlledInputValue: ''
+    };
+  }
+
+  handleSubmit() {
+    console.log('---- VALORES ----');
+    console.log(this.state.controlledInputValue);
+    console.log(this._uncontrolledText.value);
+    console.log('-----------------');
+  }
+
+  render() {
+    return(
+      <div>
+        <input
+          type='text'
+          value={this.state.controlledInputValue}
+          onChange={(evt) => this.setState({ controlledInputValue: evt.target.value })}/> <br />
+        <input type='text' defaultValue='' ref={(el) => { this._uncontrolledText = el; } }/><br />
+        <button onClick={this.handleSubmit.bind(this)}>Mandar!</button>
+      </div>
+    );
+  }
+}
